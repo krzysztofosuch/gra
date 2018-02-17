870996e2ee869f1f83645cd9eba381a74f8b5f62
Kasperejros <krajewski.kacper@gmail.com>
2018-02-17 17:44:36 +0100

    Działa Małpa

--- core/src/com/mygdx/game/Game.java
+++ core/src/com/mygdx/game/Game.java
@@ -1,33 +1,91 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.utils.Array;
 
-public class Game extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+/**
+ * See: http://blog.xoppa.com/loading-models-using-libgdx/
+ * @author Xoppa
+ */
+public class Game implements ApplicationListener {
+    public PerspectiveCamera cam;
+    public CameraInputController camController;
+    public ModelBatch modelBatch;
+    public AssetManager assets;
+    public Array<ModelInstance> instances = new Array<ModelInstance>();
+    public Environment environment;
+    public boolean loading;
+     
+    @Override
+    public void create () {
+        modelBatch = new ModelBatch();
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+         
+        cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        cam.position.set(1f, 1f, 1f);
+        cam.lookAt(0,0,0);
+        cam.near = 1f;
+        cam.far = 300f;
+        cam.update();
+ 
+        camController = new CameraInputController(cam);
+        Gdx.input.setInputProcessor(camController);
+         
+        assets = new AssetManager();
+        assets.load("zuza.g3db", Model.class);
+        loading = true;
+    }
+ 
+    private void doneLoading() {
+        Model ship = assets.get("zuza.g3db", Model.class);
+        ModelInstance shipInstance = new ModelInstance(ship); 
+        instances.add(shipInstance);
+        loading = false;
+    }
+     
+    @Override
+    public void render () {
+        if (loading && assets.update())
+            doneLoading();
+        camController.update();
+
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+        modelBatch.begin(cam);
+        modelBatch.render(instances, environment);
+        modelBatch.end();
+    }
+     
+    @Override
+    public void dispose () {
+        modelBatch.dispose();
+        instances.clear();
+        assets.dispose();
+    }
+
 	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	public void resize(int width, int height) {
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void pause() {
 	}
-	
+
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void resume() {
 	}
 }
